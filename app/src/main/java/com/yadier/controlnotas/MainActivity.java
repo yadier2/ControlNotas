8335d736c1827bb3b03fe631ea9d3a9f15c60d53
yadier caicedo <yadierwww@gmail.com>
2023-09-21 23:29:09 +0100

    config initial app

---
+++ app/src/main/java/com/yadier/controlnotas/MainActivity.java
@@ -0,0 +1,30 @@
+package com.yadier.controlnotas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashSet;
+
+
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
